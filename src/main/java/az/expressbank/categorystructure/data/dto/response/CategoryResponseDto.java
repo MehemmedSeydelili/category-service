4a1c17f13009702232872630ac757f05ad1509be
m.saydalili <m.saydalili@expressbank.az>
2023-12-15 14:50:27 +0400

    Adding fields

--- src/main/java/az/expressbank/categorystructure/data/dto/response/CategoryResponseDto.java
+++ src/main/java/az/expressbank/categorystructure/data/dto/response/CategoryResponseDto.java
@@ -1,9 +1,22 @@
 package az.expressbank.categorystructure.data.dto.response;
 
+import az.expressbank.categorystructure.data.entity.Category;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Setter
 @Getter
 public class CategoryResponseDto {
+
+    //todo change this class
+
+    private String categoryName;
+    private Category parent;
+    private String description;
+    private String categoryGroup;
+    private String keyword;
+    private LocalDateTime createAt;
+
 }
